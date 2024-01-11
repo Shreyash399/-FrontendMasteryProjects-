c50c2e26f63d2f87f837f4385251c8093d0f3aad
Shreyash Wargantwar <107786450+Shreyash399@users.noreply.github.com>
2024-01-11 14:53:36 +0530

    Add files via upload

---
+++ 01_colorchanger/shrey.js
@@ -0,0 +1,28 @@
+console.log("hitesh")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
+
+function welcome(){
+  window.open("https://c.ndtvimg.com/2023-12/k8nmu3jo_dhoni-jersey_625x300_15_December_23.jpg?im=FitAndFill,algorithm=dnn,width=1200,height=675");
+}
